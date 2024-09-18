bc620c49a6f018228a7316a9ae43f853fe8117d9
YYadorigi <leyancui@foxmail.com>
2024-09-19 03:38:55 +0800

    feat: z buffer

--- main.cpp
+++ main.cpp
@@ -6,62 +6,7 @@ const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 const int width = 800;
 const int height = 800;
-
-void line(const Vec2i &from, const Vec2i &to, TGAImage &image, const TGAColor &color)
-{
-    if (from == to)
-        return;
-
-    Vec2i start = from;
-    Vec2i end = to;
-
-    // transpose
-    bool transpose = false;
-    if (abs(start.u - end.u) < abs(start.v - end.v))
-    {
-        std::swap(start.u, start.v);
-        std::swap(end.u, end.v);
-        transpose = true;
-    }
-
-    // direction
-    if (start.u > end.u)
-        std::swap(start, end);
-
-    int dy = 2 * abs(end.v - start.v);
-    int dx = end.u - start.u;
-    int direction = (start.v < end.v ? 1 : -1);
-
-    int y = start.v;
-    int error = 0;
-
-    if (transpose)
-    {
-        for (int x = start.u; x <= end.u; x++)
-        {
-            image.set(y, x, color);
-            error += dy;
-            if (error > dx)
-            {
-                y += direction;
-                error -= dx * 2;
-            }
-        }
-    }
-    else
-    {
-        for (int x = start.u; x <= end.u; x++)
-        {
-            image.set(x, y, color);
-            error += dy;
-            if (error > dx)
-            {
-                y += direction;
-                error -= dx * 2;
-            }
-        }
-    }
-}
+const int virtual_depth = 1000000;
 
 bool inside_triangle(const Vec2i &a, const Vec2i &b, const Vec2i &c, Vec2i p)
 {
@@ -79,21 +24,43 @@ bool inside_triangle(const Vec2i &a, const Vec2i &b, const Vec2i &c, Vec2i p)
            (ab.cross_product(ap).z <= 0 && bc.cross_product(bp).z <= 0 && ca.cross_product(cp).z <= 0);
 }
 
-void triangle(const Vec2i a, const Vec2i b, const Vec2i c, TGAImage &image, const TGAColor &color)
+Vec3f barycentric_coords(const Vec2i &a, const Vec2i &b, const Vec2i &c, const Vec2i &p)
+{
+    Vec3i ab = Vec3i(b.u - a.u, c.u - a.u, a.u - p.u);
+    Vec3i ac = Vec3i(b.v - a.v, c.v - a.v, a.v - p.v);
+
+    Vec3i u = ab.cross_product(ac);
+
+    if (u.z == 0)
+        return Vec3f(-1, 1, 1);
+
+    float uz_inv = 1.f / (float)u.z;
+    return Vec3f(1.f - (u.x + u.y) * uz_inv, u.y * uz_inv, u.x * uz_inv);
+}
+
+void triangle(const Vec3i a, const Vec3i b, const Vec3i c, TGAImage &image, const TGAColor &color, int *zbuffer)
 {
     int umin, umax, vmin, vmax;
 
-    umin = std::min(std::min(a.u, b.u), c.u);
-    umax = std::max(std::max(a.u, b.u), c.u);
-    vmin = std::min(std::min(a.v, b.v), c.v);
-    vmax = std::max(std::max(a.v, b.v), c.v);
+    umin = std::min(std::min(a.x, b.x), c.x);
+    umax = std::max(std::max(a.x, b.x), c.x);
+    vmin = std::min(std::min(a.y, b.y), c.y);
+    vmax = std::max(std::max(a.y, b.y), c.y);
 
-    for (int u = umin; u <= umax; u++)
+    for (int v = vmin; v <= vmax; v++)
     {
-        for (int v = vmin; v <= vmax; v++)
+        for (int u = umin; u <= umax; u++)
         {
-            if (inside_triangle(a, b, c, Vec2i(u, v)))
-                image.set(u, v, color);
+            if (inside_triangle(Vec2i(a.x, a.y), Vec2i(b.x, b.y), Vec2i(c.x, c.y), Vec2i(u, v)))
+            {
+                Vec3f barycentric = barycentric_coords(Vec2i(a.x, a.y), Vec2i(b.x, b.y), Vec2i(c.x, c.y), Vec2i(u, v));
+                int z = a.z * barycentric.x + b.z * barycentric.y + c.z * barycentric.z;
+                if (z > zbuffer[v * width + u])
+                {
+                    image.set(u, v, color);
+                    zbuffer[v * width + u] = z;
+                }
+            }
         }
     }
 }
@@ -105,27 +72,33 @@ int main(int argc, char **argv)
 
     Vec3f light_dir(0, 0, -1); // with area infinitely large
 
+    int *zbuffer = new int[width * height];
+    memset(zbuffer, -virtual_depth, sizeof(int) * width * height);
+
     for (int i = 0; i < model->num_faces(); i++)
     {
         Vec3i face = model->get_face_index(i);
 
         Vec3f world_coords[3];
-        Vec2i screen_coords[3];
+        Vec3i screen_coords[3];
 
         for (int j = 0; j < 3; j++)
         {
             Vec3f v = model->get_vert(face.raw[j]);
             world_coords[j] = v;
-            screen_coords[j] = Vec2i((v.x + 1) / 2 * width, (v.y + 1) / 2 * height);
+            screen_coords[j] = Vec3i((v.x + 1) / 2 * width, (v.y + 1) / 2 * height, (v.z - 1) / 2 * virtual_depth);
         }
 
         Vec3f normal = (world_coords[1] - world_coords[0]).cross_product(world_coords[2] - world_coords[0]);
         normal.normalized();
 
-        float cos_theta = std::max(0.f, -normal.dot_product(light_dir));
+        float cos_theta = -normal.dot_product(light_dir);
 
-        TGAColor white_reflection(white.r * cos_theta, white.g * cos_theta, white.b * cos_theta, white.a);
-        triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, white_reflection);
+        if (cos_theta > 0)
+        {
+            TGAColor white_reflection(white.r * cos_theta, white.g * cos_theta, white.b * cos_theta, white.a);
+            triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, white_reflection, zbuffer);
+        }
     }
 
     image.flip_vertically();
